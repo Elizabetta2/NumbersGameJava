363243c96fab9f32cc994e5d8503102e16ae39db
Elizabetta2 <p-elizabeth@mail.ru>
2023-12-25 11:19:30 +0400

    com

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -26,8 +26,8 @@ public class TemperatureConverter extends JFrame{
         jTF1 = new JTextField();
         jTF2 = new JTextField();
         jB1 = new JButton("Конвертировать");
-        jB2 = new JButton();
-        jB3 = new JButton();
+        jB2 = new JButton("Очистить");
+        jB3 = new JButton("Выход");
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Конвертер температуры");
@@ -40,24 +40,100 @@ public class TemperatureConverter extends JFrame{
 
         GroupLayout jPLayout = new GroupLayout(jP1);
         jP1.setLayout(jPLayout);
-        jPLayout.setHorizontalGroup(jPLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addGroup(jPLayout.createSequentialGroup()
-                        .addGap(19, 19,19)
-                        .addComponent(jL1)
-                        .addContainerGap(23, Short.MAX_VALUE)));
+        jPLayout.setAutoCreateGaps(true);
+        jPLayout.setAutoCreateContainerGaps(true);
+
+
         jComBox1.setModel(new DefaultComboBoxModel<>(new String[]{"Цельсии", "Фаренгейт"}));
         jComBox2.setModel(new DefaultComboBoxModel<>(new String[]{"Цельсии", "Фаренгейт"}));
 
         jTF2.setEditable(false);
 
+        jPLayout.setHorizontalGroup(jPLayout.createSequentialGroup()
+                .addComponent(jTF1)
+                .addComponent(jTF2)
+                .addGroup(jPLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(jL1)
+                        .addComponent(jB1))
+        );
+
+        jPLayout.setVerticalGroup(jPLayout.createSequentialGroup()
+                .addGroup(jPLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(jTF1)
+                        .addComponent(jTF2)
+                        .addComponent(jL1))
+                        .addComponent(jB1));
+
+
+
         jB1.setFont(new Font("Segoe UI", 3, 14));
         jB1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-             //   jB1ActionPerformed(e);
+                jB1ActionPerformed(e);
             }
         });
+
+        jB2.setFont(new Font("Segoe UI", 3, 14));
+        jB2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                   jB2ActionPerformed(e);
+            }
+        });
+
+        jB3.setFont(new Font("Segoe UI", 3, 14));
+        jB3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                   jB3ActionPerformed(e);
+            }
+        });
+
+       getContentPane().add(jP1);
+
+
+        pack();
+
     }
 
+    private void jB3ActionPerformed(ActionEvent evt) {
+        System.exit(0);
+    }
+
+    private void jB2ActionPerformed(ActionEvent evt) {
+        jTF1.setText("");
+        jTF2.setText("");
+    }
+
+    private void jB1ActionPerformed(ActionEvent evt){
+        String val1 = (String) jComBox1.getSelectedItem();
+        String val2 = (String) jComBox2.getSelectedItem();
+
+        if (val1.equals("Цельсия") && val2.equals("Фаренгейт")) {
+            double cel = Double.parseDouble(jTF1.getText());
+            double fah = (double) ((9.0/5.0)*cel + 32);
+
+            jTF2.setText(String.valueOf(fah));
+        } else if (val1.equals("Цельсия") && val2.equals("Цельсия")) {
+            double c = Double.parseDouble(jTF1.getText());
+            jTF2.setText(String.valueOf(c));
+        }
+
+        if (val1.equals("Фаренгейт") && val2.equals("Цельсия")) {
+            double f = Double.parseDouble(jTF1.getText());
+            double c = (double) ((f - 32) *(5.0/9.0));
+
+            jTF2.setText(String.valueOf(Math.round(c)));
+        } else if (val1.equals("Фаренгейт") && val2.equals("Фаренгейт")) {
+            double f = Double.parseDouble(jTF1.getText());
+            jTF2.setText(String.valueOf(Math.round(f)));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new TemperatureConverter().setVisible(true);
+    }
 
 }
